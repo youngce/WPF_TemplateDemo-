17526536c18580808742ce58ddfc6ea034865673
YangTseChung <youngce0918@gmail.com>
2013-10-10 14:59:14 +0800

    Initial

---
+++ ControlTemplateAndDataTemplate/ViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ReactiveUI;
+using ReactiveUI.Xaml;
+
+namespace ControlTemplateAndDataTemplate
+{
+    class ViewModel:ReactiveObject
+    {
+        private string _MyText;
+        public string MyText { get { return _MyText; } set { this.RaiseAndSetIfChanged(x => x.MyText, value); } }
+        public ReactiveCommand MyButton { get; set; }
+        public ViewModel()
+        {
+            MyText = "Mark";
+            MyButton=new ReactiveCommand();
+            MyButton.Subscribe(ButtonFunc);
+
+        }
+
+        public void ButtonFunc(object o)
+        {
+            MyText += "Mark";
+
+        }
+    }
+}
